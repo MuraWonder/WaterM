91f494872732a0e351d9dbfdbe12425b7e43682c
MuraWonder <101500523+MuraWonder@users.noreply.github.com>
2022-09-15 22:40:25 +0800

    9/15 多一個gallary視窗做測試

--- Assets/script/Gallery.cs
+++ Assets/script/Gallery.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Gallery : MonoBehaviour
 {   public GameObject winS01;
     public GameObject winB01;
+    public GameObject winS02;
+    public GameObject winB02;
 
     // Start is called before the first frame update
     void Start()
@@ -24,5 +27,17 @@ public class Gallery : MonoBehaviour
     public void bM01(){
         winS01.SetActive(true); //小視窗回來
         winB01.SetActive(false); //大視窗不見
+    }
+     public void sM02(){
+        winB02.SetActive(true); //大視窗出現
+        winS02.SetActive(false); //小視窗不見
+    }
+    public void bM02(){
+        winS02.SetActive(true); //小視窗回來
+        winB02.SetActive(false); //大視窗不見
+    }
+     public void Menu()
+    {
+        SceneManager.LoadScene("Menu");
     }
 }
